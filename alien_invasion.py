8c425c78ac2de5bb8dfc6249ee973ab578f39833
tithoft <99927615+tithoft@users.noreply.github.com>
2025-10-23 07:42:13 -0400

    Create method to display current level on screen.
    Level dynamically increases after aliens cleared.

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,6 +81,7 @@ class AlienInvasion:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.sb.prep_score()
+            self.sb.prep_level()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
@@ -145,6 +146,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level =+ 1
+            self.sb.prep_level()
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
