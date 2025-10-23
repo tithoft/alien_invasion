da486119c6c1a37bf3591345d31ffc13edbeac13
tithoft <99927615+tithoft@users.noreply.github.com>
2025-10-23 07:21:42 -0400

    Dynamically increases score with increased level.
    Scores formatted to round to nearest 10th.

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,7 @@ class AlienInvasion:
 
             # Reset the game statistics.
             self.stats.reset_stats()
+            self.sb.prep_score()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
@@ -132,6 +133,11 @@ class AlienInvasion:
         """Respond to bullet-alien collisions."""
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
